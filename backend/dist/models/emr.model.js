775c455b21b78371fb3e94a23a97cfb2ee1a9642
Sujal Raj <sujalraj2064@gmail.com>
2025-12-02 12:41:09 +0530

    Initial commit

---
+++ backend/dist/models/emr.model.js
@@ -0,0 +1,40 @@
+import mongoose, { Document, Schema } from "mongoose";
+const emrSchema = new mongoose.Schema({
+    doctorId: {
+        type: Schema.Types.ObjectId,
+        ref: "Doctor",
+        default: null,
+    },
+    aadhar: {
+        type: Number,
+        default: null,
+    },
+    allergies: {
+        type: [String],
+        default: [],
+    },
+    diseases: {
+        type: [String],
+        default: [],
+    },
+    pastSurgeries: {
+        type: [String],
+        default: [],
+    },
+    currentMedications: {
+        type: [String],
+        default: [],
+    },
+    reports: {
+        type: [String], // stored as file URL path
+        default: [],
+    },
+    prescriptionId: {
+        type: [Schema.Types.ObjectId],
+        ref: "Prescription",
+        default: [],
+    },
+}, { timestamps: true });
+const EMRModel = mongoose.model("EMR", emrSchema, "EMR");
+export default EMRModel;
+//# sourceMappingURL=emr.model.js.map
